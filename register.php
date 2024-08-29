1c3b722e79e986a86363e0e13240c5af6156ad8d
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-28 22:37:56 -0500

    Create register.php

---
+++ register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Register</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+</head>
+<body>
+    <?php
+
+
+    ?>
+    
+</body>
+</html>
