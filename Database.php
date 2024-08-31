46fa5b8932dbe8b4c8c66dff91d630bc5d397fda
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-31 14:10:28 -0500

    refactor db to pass val

--- Database.php
+++ Database.php
@@ -1,31 +1,32 @@
 <?php
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    public $conn;
 
-class Database
-{
-
-    private ?PDO $conn = null;
-    public function __construct(
-        private string $host,
-        private string $name,
-        private string $user,
-        private string $password
-    ){
-
+    public function __construct($host, $db_name, $username, $password) {
+        $this->host = $host;
+        $this->db_name = $db_name;
+        $this->username = $username;
+        $this->password = $password;
     }
 
-    public function getConnection(): ?PDO{
-        try{
-            if($this -> conn === null){
-                $this -> conn = new PDO("mysql:host = $this->host; dbname = $this->name", $this-> user, $this->password);
-                $this -> conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $this -> conn -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-                $this -> conn -> setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
-            }
-            return $this -> conn;
+    public function getConnection() {
+        $this->conn = null;
 
-        }catch(PDOException $e){
-            echo "Connection failed: " . $e->getMessage();
-            return null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name}",
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
         }
+
+        return $this->conn;
     }
 }
