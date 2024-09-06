b371f223e9c6a6a0a9b594524cda2edf0034a97a
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-05 20:21:49 -0500

    create pages

---
+++ JWT.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+    header('Location: login.php');
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>NAME | Base 64</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+</head>
+<body>
+
+<h1>JWT Tool</h1>
+</body>
+</html>
