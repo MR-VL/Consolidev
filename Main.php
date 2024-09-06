afdfc89b0c38c7e417d8d2723b5f23530b23700b
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-05 20:04:07 -0500

    protect page using session

--- Main.php
+++ Main.php
@@ -1,15 +1,21 @@
-<!DOCTYPE html>
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header('Location: login.php');
+    }
 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <title>Calender</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
 </head>
 <body>
-    <?php
 
-
-    ?>
     <h1> LOGGED IN SUCCESS</h1>
 </body>
 </html>
