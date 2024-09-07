0c96537d66c1a0aa4bb6592e325e0af679d7f13f
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-06 19:53:39 -0500

    fixed display

--- base64.php
+++ base64.php
@@ -16,47 +16,57 @@ else{
 
 if($type == 1){
     $filtered = base64_encode($input);
-    echo "<div><h2>Encoded Base 64</h2></div><br>";
+    $display = "<div style='color: #00008B'><h2>Encoded Base 64:</h2><br> <h2>$filtered</h2></div>";
 }
 else{
     $filtered = base64_decode($input);
-    echo "<div><h2>Decoded Base 64</h2></div><br>";
+    $display = "<div style='color: #00008B'><h2>Decoded Base 64:</h2><br> <h2>$filtered</h2><br>";
 }
 
-echo "<br><div><h2>$filtered</h2></div>";
-
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>NAME | Base 64</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="styles.css">
+    <style>
+        .container {
+            display: flex;
+            flex-direction: row;
+            gap: 20px;
+            max-width: 80vw;
+        }
+    </style>
 </head>
 <body>
-<div style="max-width: 40vw;">
-    <h1>result</h1>
-</div>
-<div class="form" style="margin-left: 10px; max-width: 40vw">
-    <form action="base64.php" method="post">
-        <label for="base64">Enter a value and choose to encode or decode Base 64</label>
-        <div style="display: inline-flex">
-            <input type="radio" id="encode" name="ans" value="encode" required>
-            <label for="encode">Encode</label>
-        </div>
-        <br>
-        <div style="display: inline-flex; margin-top: 3vh">
-            <input type="radio" id="decode" name="ans" value="decode" required>
-            <label for="decode">Decode</label>
-        </div>
+<div class="container">
+    <div class="form">
+        <form action="base64.php" method="post">
+            <label for="base64">Enter a value and choose to encode or decode Base 64</label>
+            <br>
+            <div style="display: inline-flex">
+                <input type="radio" id="encode" name="ans" value="encode" required>
+                <label for="encode">Encode</label>
+            </div>
+            <br>
+            <div style="display: inline-flex; margin-top: 3vh">
+                <input type="radio" id="decode" name="ans" value="decode" required>
+                <label for="decode">Decode</label>
+            </div>
+            <input style="height: 20vh" type="text" id="base64" name="base64" placeholder="Type here" required><br><br>
+            <input type="submit" value="Convert">
+        </form>
+    </div>
 
 
-        <input style="height: 20vh" type ="text" id="base64" name="base64" placeholder="Type here" required><br><br>
-
-        <input type="submit" value="Convert">
-    </form>
+    <div class="form">
+        <?php
+            if(!empty($display)){
+                echo $display;
+            }
+        ?>
+    </div>
 </div>
 </body>
 </html>
