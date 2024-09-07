24e61bc0f8eb5ddccdbe685afcb4992ec96f3c61
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-06 19:41:04 -0500

    logic works. needs styling

--- base64.php
+++ base64.php
@@ -6,6 +6,7 @@ if(!isset($_SESSION['username'])) {
 $input = filter_input(INPUT_POST, "base64", FILTER_SANITIZE_SPECIAL_CHARS);
 
 $type = $_POST["ans"];
+
 if($type == "encode"){
     $type = 1;
 }
@@ -15,14 +16,14 @@ else{
 
 if($type == 1){
     $filtered = base64_encode($input);
-    echo "<h2>Encoded Base 64</h2>";
+    echo "<div><h2>Encoded Base 64</h2></div><br>";
 }
 else{
     $filtered = base64_decode($input);
-    echo "<h2>Decoded Base 64</h2>";
+    echo "<div><h2>Decoded Base 64</h2></div><br>";
 }
 
-echo "<h2>$filtered</h2>";
+echo "<br><div><h2>$filtered</h2></div>";
 
 
 ?>
@@ -35,8 +36,11 @@ echo "<h2>$filtered</h2>";
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<div class="form">
-    <form action="base64.php">
+<div style="max-width: 40vw;">
+    <h1>result</h1>
+</div>
+<div class="form" style="margin-left: 10px; max-width: 40vw">
+    <form action="base64.php" method="post">
         <label for="base64">Enter a value and choose to encode or decode Base 64</label>
         <div style="display: inline-flex">
             <input type="radio" id="encode" name="ans" value="encode" required>
@@ -49,7 +53,7 @@ echo "<h2>$filtered</h2>";
         </div>
 
 
-        <input style="height: 40vh" type ="text" id="base64" name="base64" placeholder="Type here" required><br><br>
+        <input style="height: 20vh" type ="text" id="base64" name="base64" placeholder="Type here" required><br><br>
 
         <input type="submit" value="Convert">
     </form>
