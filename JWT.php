7ee75629912a8fc3eaeaad8621ef74377f5708d3
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-21 18:57:04 -0500

    init jwt

--- JWT.php
+++ JWT.php
@@ -8,15 +8,44 @@ if(!isset($_SESSION['username'])){
 
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Consolidev | Base 64</title>
+    <title>Consolidev | JWT</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+    <link rel="stylesheet" href="styles.css">
+    <style>
+        .container {
+            display: flex;
+            flex-direction: row;
+            gap: 20px;
+            max-width: 80vw;
+        }
+    </style>
 </head>
 <body>
+<div class="container">
 
-<h1>JWT Tool</h1>
+    <div class="form">
+        <form action="hashing.php" method="post">
+
+
+
+        </form>
+    </div>
+
+
+    <div class="form" style="word-wrap: break-word">
+
+        <a href="hashingHistory.php">
+            <button class="btn" style="width:auto">View History</button>
+        </a>
+        <?php
+        if(!empty($display)){
+            echo $display;
+        }
+        ?>
+    </div>
+</div>
 </body>
-</html>
