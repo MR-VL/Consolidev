37531d0c4a6bd33a60a5f2718e51ab133eb6fb14
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-21 15:16:56 -0500

    add db files

--- SQL_DUMP/consolidev.sql
+++ SQL_DUMP/consolidev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 21, 2024 at 07:55 PM
+-- Generation Time: Sep 21, 2024 at 08:16 PM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.18
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `consolidev`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `base64`
+--
+
+DROP TABLE IF EXISTS `base64`;
+CREATE TABLE IF NOT EXISTS `base64` (
+  `TransactionID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `type` varchar(6) NOT NULL,
+  `original` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `opposite` text NOT NULL,
+  PRIMARY KEY (`TransactionID`),
+  KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `base64`
+--
+
+INSERT INTO `base64` (`TransactionID`, `username`, `type`, `original`, `opposite`) VALUES
+(1, 'test', 'encode', 'abcd', 'YWJjZA=='),
+(2, 'test', 'decode', 'aGVsbG8=', 'hello'),
+(3, 'test', 'encode', 'testing', 'dGVzdGluZw=='),
+(4, 'test', 'decode', 'dGVzdGluZyBoZWxsb29vbz8/', 'testing helloooo??');
 
 -- --------------------------------------------------------
 
@@ -56,15 +83,66 @@ INSERT INTO `hashing` (`TransactionID`, `username`, `algorithm`, `original`, `op
 (11, 'test', 'crc32', 'crc32', '6add5dc6'),
 (12, 'test', 'adler32', 'adler', '05fa0209');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `json`
+--
+
+DROP TABLE IF EXISTS `json`;
+CREATE TABLE IF NOT EXISTS `json` (
+  `TransactionID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `Input` text NOT NULL,
+  `errorCount` int NOT NULL,
+  `errors` text NOT NULL,
+  PRIMARY KEY (`TransactionID`),
+  KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Fname` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Lname` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `Fname`, `Lname`, `password`) VALUES
+('test', 'Test', 'Test', '$2y$10$l.LJQnOcdpdH50X2hTYUu.c2QjMKqmZbZw461VYr6sHw0/RLLNQGC');
+
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `base64`
+--
+ALTER TABLE `base64`
+  ADD CONSTRAINT `base64_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `hashing`
 --
 ALTER TABLE `hashing`
   ADD CONSTRAINT `hashing_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `json`
+--
+ALTER TABLE `json`
+  ADD CONSTRAINT `json_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
