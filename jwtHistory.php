ffdc0f62d7d0189b262b1d7f316639acdcc860ff
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-22 18:29:12 -0500

    jwt history return

--- jwtHistory.php
+++ jwtHistory.php
@@ -1,3 +1,27 @@
+<?php
+    require_once "init.php";
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header('Location: login.php');
+    }
+    $username = $_SESSION['username'];
+
+    $sql = "SELECT * FROM jwt WHERE username = :username";
+    global $connect;
+
+    try{
+        $stmt = $connect->prepare($sql);
+        $stmt -> bindParam(':username', $username);
+        $stmt -> execute();
+        $userHistory = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }catch (PDOException $e){
+        echo "<h1>Query failed:" .$e->getMessage()."</h1>";
+        ob_end_flush();
+        exit();
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,15 +38,15 @@
     </a>
     <table border="1" style="width: 100%; text-align: center; border-collapse: collapse;">
         <tr>
-            <th style="padding: 10px;">Type</th>
             <th style="padding: 10px;">Original</th>
             <th style="padding: 10px;">Opposite</th>
         </tr>
-
+        <?php
+        foreach ($userHistory as $row){
+            echo "<tr><td style='padding: 10px;'>{$row['original']}</td><td style='padding: 10px;'>{$row['opposite']}</td></tr>";
+        }
+        ?>
     </table>
-
-
-
 </div>
 </body>
 </html>
