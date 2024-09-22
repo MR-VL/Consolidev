86761d31dab38f70a3900d002c19754e8990f937
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-09-22 18:12:28 -0500

    html part

--- jwtHistory.php
+++ jwtHistory.php
@@ -1,1 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Consolidev | JWT History</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+<div class="form" style="word-wrap: break-word">
+
+    <a href="JWT.php">
+        <button class="btn">Go back</button><br>
+    </a>
+    <table border="1" style="width: 100%; text-align: center; border-collapse: collapse;">
+        <tr>
+            <th style="padding: 10px;">Type</th>
+            <th style="padding: 10px;">Original</th>
+            <th style="padding: 10px;">Opposite</th>
+        </tr>
+
+    </table>
+
+
+
+</div>
+</body>
+</html>
