d8e70ed403ace323e4b711ac5b568bc34ff4830c
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-10-03 13:18:00 -0500

    add comments

--- base64history.php
+++ base64history.php
@@ -1,4 +1,5 @@
 <?php
+//See Base64.php for explanation on what this does
 require_once 'init.php';
 session_start();
 if(!isset($_SESSION['username'])) {
@@ -6,16 +7,22 @@ if(!isset($_SESSION['username'])) {
 }
 $username = $_SESSION['username'];
 
+//sql statement to pull all created by user
 $sql = "SELECT * FROM base64 WHERE username = :username";
 global $connect;
 
+//you need this try block in case user has no entries
 try{
     $stmt = $connect->prepare($sql);
     $stmt -> bindParam(':username', $username);
     $stmt->execute();
+
+    //fetches all the stuff the user created
     $userHistory = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 }catch(PDOException $e){
+    //You can copy this entire catch block as is, in case above fails for whatever reason it will return the error
+    //message to the user and exit
     echo "<h1>Query failed:" .$e->getMessage()."</h1>";
     ob_end_flush();
     exit();
@@ -33,6 +40,7 @@ try{
 <body>
 <div class="form" style="word-wrap: break-word">
 
+    <!--This is the HTML it needs a lot of styling due to being so zoomed in for some reason.... -->
     <a href="base64.php">
         <button class="btn">Go back</button><br>
     </a>
@@ -43,6 +51,8 @@ try{
             <th style="padding: 10px;">Opposite</th>
         </tr>
         <?php
+        //iterate through the userhistory and display each line in a table format, you can basically just copy it as it and only chage the variable in
+        //$row['CHANGEME'] to whatever your tables column names are, you should ommit the username because it will be the same for the user and is unneccessary
         foreach ($userHistory as $row){
         echo "<tr><td style='padding: 10px;'>{$row['type']}</td><td style='padding: 10px;'>{$row['original']}</td><td style='padding: 10px;'>{$row['opposite']}</td></tr>";
         }
