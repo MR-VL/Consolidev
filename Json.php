d44bfb03fc48bc15eede0f8a16a5ac21bfff5115
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-10-13 17:52:57 -0500

    html

--- Json.php
+++ Json.php
@@ -8,15 +8,46 @@ if(!isset($_SESSION['username'])){
 
 ?>
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Consolidev | Base 64</title>
+    <title>Consolidev | JSON</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+    <link rel="stylesheet" href="styles.css">
+    <style>
+        .container {
+            display: flex;
+            flex-direction: row;
+            gap: 20px;
+            max-width: 80vw;
+        }
+        textarea {
+            width: 100%;
+            height: 50vh;
+            word-wrap: break-word;
+        }
+    </style>
 </head>
 <body>
+<div class="container">
 
-<h1>JSON page</h1>
+    <div class="form">
+        <form action="Json.php" method="post">
+            <h1 style="color: #00008B">Validate Json</h1>
+            <label for="input"></label>
+            <textarea id="input" name="input" placeholder="Enter JSON" required></textarea><br><br>
+            <input type="submit" value="Validate">
+        </form>
+    </div>
+
+    <div class="form" style="word-wrap: break-word">
+        <?php
+        if(!empty($display)){
+            echo $display;
+        }
+        ?>
+    </div>
+</div>
 </body>
 </html>
