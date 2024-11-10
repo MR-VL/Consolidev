9915d2c961a38419305150dac2123c2b30309659
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-11-10 16:52:37 -0600

    add cache control global

--- timestampconverter.php
+++ timestampconverter.php
@@ -2,9 +2,6 @@
 // Initialize connection and session check
 require_once 'init.php';
 session_start();
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
 
 if (!isset($_SESSION['username'])) {
     header('Location: login.php');
